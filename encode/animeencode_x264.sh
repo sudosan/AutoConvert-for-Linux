944c2a6a80a3d72f3adafedd1276895d16933259
sudosan <cream.stew.007+586@gmail.com>
2015-02-14 01:41:06 +0900

    fix

--- encode/animeencode_x264.sh
+++ encode/animeencode_x264.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 HandBrakeCLI -i $1 -o $1.mp4 -e x264 --x264-preset=slower --x264-tune=animation --h264-profile=high -q 20 --rate 23.976 --cfr -a 1 -E copy -6 auto -w 1280 -l 720 --crop 0:0:0:0 --decomb --detelecine --deinterlace
